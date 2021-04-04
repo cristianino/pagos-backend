1cdb905897906c062bf2b81a4f156df78dcd9cde
Cristian Niño <cristian_david113@hotmail.com>
2021-04-04 16:03:44 -0500

    Response "welcome to your system payments" to root route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response(200, 'welcome to your system payments');
 });
 
 Auth::routes();
